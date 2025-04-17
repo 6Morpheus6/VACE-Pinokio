2901883500c9d7a4e88c99105c6333b76f8aa213
6Morpheus6 <wanton-decay@gmx.net>
2025-04-17 18:22:20 +0200

    skip smudge, install triton, groundingdino-py, custom requirements

--- install.js
+++ install.js
@@ -18,7 +18,7 @@ module.exports = {
           venv: "env",                // Edit this to customize the venv folder path
           path: "app",                // Edit this to customize the path to start the shell from
           // xformers: true   // uncomment this line if your project requires xformers
-          // triton: true   // uncomment this line if your project requires triton
+          triton: true   // uncomment this line if your project requires triton
           // sageattention: true   // uncomment this line if your project requires sageattention
         }
       }
@@ -27,13 +27,16 @@ module.exports = {
     {
       method: "shell.run",
       params: {
+        env: {
+          "GIT_LFS_SKIP_SMUDGE": "1"
+        },
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "uv pip install -r requirements.txt",
+          "uv pip install -r ../requirements.txt",
           "uv pip install wan@git+https://github.com/Wan-Video/Wan2.1",
           "uv pip install ltx-video@git+https://github.com/Lightricks/LTX-Video@ltx-video-0.9.1 sentencepiece --no-deps",
-          "pip install -r requirements/annotator.txt"
+          "uv pip install -r ../annotator.txt"
         ]
       }
     },
@@ -49,28 +52,28 @@ module.exports = {
     {
       method: "hf.download",
       params: {
-        path: "app",
+        path: "app/models",
         "_": [ "ali-vilab/VACE-Annotators" ],
         "exclude": '".gittatributes" "*.md"',
-        "local-dir": "models",
+        "local-dir": "VACE-Annotators",
       }
     },
     {
       method: "hf.download",
       params: {
-        path: "app",
+        path: "app/models",
         "_": [ "ali-vilab/VACE-Wan2.1-1.3B-Preview" ],
         "exclude": '".gittatributes" "*.md" "*.txt"',
-        "local-dir": "models",
+        "local-dir": "VACE-Wan2.1-1.3B-Preview",
       }
     },
     {
       method: "hf.download",
       params: {
-        path: "app",
+        path: "app/models",
         "_": [ "ali-vilab/VACE-LTX-Video-0.9" ],
         "exclude": '".gittatributes" "*.md" "*.txt"',
-        "local-dir": "models",
+        "local-dir": "VACE-LTX-Video-0.9",
       }
     },
   ]
