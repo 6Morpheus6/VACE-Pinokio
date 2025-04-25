38f2792a2b6803056b134c254dc69ad9ae678a1d
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-04-25 18:33:29 +0200

    switch to fork

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/ali-vilab/VACE app",
+          "git clone https://github.com/6Morpheus6/VACE app",
         ]
       }
     },
